9002da8c48c3bac56c93b39dd2a20ed9a07b3609
Hendi Santika <hendisantika@gmail.com>
2018-02-24 21:38:29 +0700

    #3 - Add Main App

---
+++ src/main/java/com/hendisantika/reactive/stream/java9/AppMain.java
@@ -0,0 +1,17 @@
+package com.hendisantika.reactive.stream.java9;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : reactive-stream-java9
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 24/02/18
+ * Time: 21.37
+ * To change this template use File | Settings | File Templates.
+ */
+public class AppMain {
+    public static void main(String[] args) {
+
+    }
+}
