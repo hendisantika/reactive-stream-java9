9aa72de8ffaf210e1f6811a4a3fac6bace347bfd
Hendi Santika <hendisantika@gmail.com>
2018-02-25 06:36:10 +0700

    #6 - Add Property Config

---
+++ src/main/java/com/hendisantika/reactive/stream/java9/config/Property.java
@@ -0,0 +1,27 @@
+package com.hendisantika.reactive.stream.java9.config;
+
+import javax.enterprise.util.Nonbinding;
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : reactive-stream-java9
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/02/18
+ * Time: 06.35
+ * To change this template use File | Settings | File Templates.
+ */
+@Qualifier
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.TYPE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+public @interface Property {
+    @Nonbinding String value() default "";
+
+    @Nonbinding boolean required() default true;
+}
